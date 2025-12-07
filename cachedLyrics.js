26c80e46de39aa2b685e826a8e281f6feb228e80
Random-Dummy <157031761+Random-Dummy@users.noreply.github.com>
2025-12-07 23:17:18 +0800

    a

--- cachedLyrics.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cachedLyricsSchema = new mongoose.Schema({
-  spotifyTrackId: String,    
-  lyrics: String,            
-  cachedAt: Date             
-});
-
-module.exports = mongoose.model('CachedLyrics', cachedLyricsSchema);
