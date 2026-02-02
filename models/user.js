d488d0878bf352096eff8a17e0e699ff20b21f97
Random-Dummy <157031761+Random-Dummy@users.noreply.github.com>
2026-02-02 20:09:04 +0800

    a

--- models/user.js
+++ models/user.js
@@ -1,16 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: String,
     email: { type: String, required: true, unique: true },
-    password: String,
-    pfp: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('Users', userSchema);
-
-    // spotify: {
-    //     spotifyUserId: String,
-    //     accessToken: String,
-    //     refreshToken: String
-    // },
+    
+})
